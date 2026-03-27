e9782af4197a747f61b04c0061904ccda92b633e
marcoantonioni <antonioni.marco@gmail.com>
2026-03-27 06:26:34 +0100

    update

--- scripts/wfps-deploy.sh
+++ scripts/wfps-deploy.sh
@@ -19,6 +19,35 @@ _CLR_YELLOW="\033[1;33m"   #'1;32' is Yellow's ANSI color code
 _CLR_BLUE="\033[0;34m"   #'0;34' is Blue's ANSI color code
 _CLR_NC="\033[0m"
 
+#--------------------------------------------------------
+_INST_TMP_FOLDER="/tmp"
+setTemporaryFolder () {
+  _OK=0
+  _ERR_MSG_FOLDER="is a folder"
+  _ERR_MSG_PERMISSIONS=""
+  if [[ ! -z "${CP4BA_INST_TMP_FOLDER}" ]]; then
+    if [[ -d "${CP4BA_INST_TMP_FOLDER}" ]]; then
+      if [[ -r "${CP4BA_INST_TMP_FOLDER}" ]] && [[ -w "${CP4BA_INST_TMP_FOLDER}" ]]; then 
+        _OK=1
+      else
+        _ERR_MSG_PERMISSIONS=", you have not rights to read and/or write"
+        _OK=-1
+      fi
+    else
+      _ERR_MSG_FOLDER="is NOT a folder"
+    fi
+
+    if [[ $_OK -lt 1 ]]; then
+      echo -e "${_CLR_RED}[✗] ERROR '${_CLR_YELLOW}${CP4BA_INST_TMP_FOLDER}${_CLR_RED}' is not a valid temporary folder, check if it is a folder or if you have write permissions !${_CLR_NC}"
+      echo -e "${_CLR_RED}'${_CLR_YELLOW}${CP4BA_INST_TMP_FOLDER}${_CLR_RED}' ${_ERR_MSG_FOLDER}${_ERR_MSG_PERMISSIONS}${_CLR_NC}"
+      exit 1
+    fi
+    export _INST_TMP_FOLDER="${CP4BA_INST_TMP_FOLDER}"
+  fi
+  echo -e "${_CLR_GREEN}Running with temporary folder '${_CLR_YELLOW}${_INST_TMP_FOLDER}${_CLR_GREEN}'${_CLR_NC}"
+
+}
+
 usage () {
   echo ""
   echo -e "${_CLR_GREEN}usage: $_me
@@ -471,7 +500,7 @@ executeExportVars () {
 # Wait for FIX
 __workaround () {
   _HOST=$(oc get route cpd -o jsonpath='{.spec.host}') 1>/dev/null
-  _PATCH_FILE="/tmp/wfps-patch-$RANDOM.yaml"
+  _PATCH_FILE="${_INST_TMP_FOLDER}/wfps-patch-$RANDOM.yaml"
   _PROPS='<properties> 
           <server merge="mergeChildren">
             <portal merge="mergeChildren">
@@ -499,6 +528,8 @@ echo "*************************************"
 echo "****** WfPS Runtime Deployment ******"
 echo "*************************************"
 echo "Using config file: "${CONFIG_FILE}
+
+setTemporaryFolder
 
 if [[ ! -f "${_CFG}" || ! -f "${_ENV_CFG}" ]]; then
   echo "Configuration file not found -c [${_CFG}] -e [${_ENV_CFG}]"
--- scripts/wfps-federate.sh
+++ scripts/wfps-federate.sh
@@ -8,6 +8,42 @@ _me=$(basename "$0")
 _ENV_CFG=""
 
 #--------------------------------------------------------
+_CLR_RED="\033[0;31m"   #'0;31' is Red's ANSI color code
+_CLR_GREEN="\033[0;32m"   #'0;32' is Green's ANSI color code
+_CLR_YELLOW="\033[1;33m"   #'1;32' is Yellow's ANSI color code
+_CLR_BLUE="\033[0;34m"   #'0;34' is Blue's ANSI color code
+_CLR_NC="\033[0m"
+
+#--------------------------------------------------------
+_INST_TMP_FOLDER="/tmp"
+setTemporaryFolder () {
+  _OK=0
+  _ERR_MSG_FOLDER="is a folder"
+  _ERR_MSG_PERMISSIONS=""
+  if [[ ! -z "${CP4BA_INST_TMP_FOLDER}" ]]; then
+    if [[ -d "${CP4BA_INST_TMP_FOLDER}" ]]; then
+      if [[ -r "${CP4BA_INST_TMP_FOLDER}" ]] && [[ -w "${CP4BA_INST_TMP_FOLDER}" ]]; then 
+        _OK=1
+      else
+        _ERR_MSG_PERMISSIONS=", you have not rights to read and/or write"
+        _OK=-1
+      fi
+    else
+      _ERR_MSG_FOLDER="is NOT a folder"
+    fi
+
+    if [[ $_OK -lt 1 ]]; then
+      echo -e "${_CLR_RED}[✗] ERROR '${_CLR_YELLOW}${CP4BA_INST_TMP_FOLDER}${_CLR_RED}' is not a valid temporary folder, check if it is a folder or if you have write permissions !${_CLR_NC}"
+      echo -e "${_CLR_RED}'${_CLR_YELLOW}${CP4BA_INST_TMP_FOLDER}${_CLR_RED}' ${_ERR_MSG_FOLDER}${_ERR_MSG_PERMISSIONS}${_CLR_NC}"
+      exit 1
+    fi
+    export _INST_TMP_FOLDER="${CP4BA_INST_TMP_FOLDER}"
+  fi
+  echo -e "${_CLR_GREEN}Running with temporary folder '${_CLR_YELLOW}${_INST_TMP_FOLDER}${_CLR_GREEN}'${_CLR_NC}"
+
+}
+
+#--------------------------------------------------------
 # read command line params
 while getopts c:e: flag
 do
@@ -42,7 +78,7 @@ source $_SCRIPT_DIR/oc-utils.sh
 
 __workaround () {
   _HOST=$(oc get route cpd -o jsonpath='{.spec.host}') 1>/dev/null
-  _PATCH_FILE="/tmp/wfps-patch-$RANDOM.yaml"
+  _PATCH_FILE="${_INST_TMP_FOLDER}/wfps-patch-$RANDOM.yaml"
   _PROPS='<properties> 
           <server merge="mergeChildren">
             <portal merge="mergeChildren">
